95fd293674fb3446c90d8344fa354b009a762a68
1532444 <1532444@2F24-26>
2016-11-02 12:13:50 -0400

    Block is working -- Working on a Normal play round

--- TicTacToe/TicTacToe/IAEasy.cs
+++ TicTacToe/TicTacToe/IAEasy.cs
@@ -22,8 +22,20 @@ namespace TicTacToe
          * methods to make the EasyIA play
          * 
          */ 
-        public int Play()
+        public int Play(Button[] buttonArray, int[,] winCombination)
         {
+            int action;
+            action = Block(buttonArray, winCombination);
+
+            if (action == -1)
+            {
+                Console.WriteLine("Block Gave -1 Bouuh");
+            }
+
+
+
+
+
             return 1;
         }
 
@@ -31,11 +43,8 @@ namespace TicTacToe
 
         public int Block(Button [] buttonArray, int[,] winCombination)
         {   
-            bool result = false;
-
-
             //Loop looking for a POSSIBLE WIN
-            for (int i = 0; i < 8 && !result; i++)
+            for (int i = 0; i < 8 ; i++)
             {
                 int a = winCombination[i, 0];
                 int b = winCombination[i, 1];
@@ -45,39 +54,62 @@ namespace TicTacToe
                 Button but2 = buttonArray[b];
                 Button but3 = buttonArray[c];
 
-                if (but1.Content == "X" && but2.Content == "X" && but3.Content == "") // Look if there is a POSSIBLE WIN
+                // Look if there is a POSSIBLE WIN
+                if (but1.Content == "X" && but2.Content == "X" && but3.Content == "") 
                 {
                     Console.WriteLine("Combination X, X, ''\t: " + i);
-                    return placeYourCircle(i, buttonArray);    // if they are empty that mean nothing happen this turn
+                    return indexOfBlock(i, buttonArray);    //Will look which index of the button will be EMPTY
                 }
 
                 if (but1.Content == "X" && but2.Content == "" && but3.Content == "X") 
                 {
                     Console.WriteLine("Combination X, '', X\t: " + i);
-                    return placeYourCircle(i, buttonArray);    // if they are empty that mean nothing happen this turn
+                    return indexOfBlock(i, buttonArray);    //Will look which index of the button will be EMPTY
                 }
-
 
                 if (but1.Content == "" && but2.Content == "X" && but3.Content == "X")
                 {
                     Console.WriteLine("Combination '', X, X\t: " + i);
-                    return placeYourCircle(i, buttonArray);     // if they are empty that mean nothing happen this turn
+                    return indexOfBlock(i, buttonArray);     //Will look which index of the button will be EMPTY
                 }
 
             }
-           
+
+            Console.WriteLine("Return -1!");
             return -1;
         }
 
-        private int placeYourCircle(int index, Button[] buttonArray)
+
+        /**
+         * This method will look the EXACT index of the
+         * empty spot and return it.
+         * If I do not found it I will return -1 and deal with it 
+         * in the Play method.
+         */ 
+        private int indexOfBlock(int index, Button[] buttonArray)
         {
+            int foundEmpty = -1;
+
             for (int i = 0; i < winCombination.GetLength(1); i++)
             {
                 Console.WriteLine("This one? : " + winCombination[index, i] + "\tL: " + winCombination.GetLength(1));
                 if (buttonArray[winCombination[index, i]].Content == "")
+                {
                     Console.WriteLine("This one Is EMPTUUU!! : " + winCombination[index, i]);
+                    foundEmpty = i;
+                }
             }
-            return 1;
+            return foundEmpty;
+        }
+
+        private void playBlock()
+        {
+
+        }
+
+        private void normalPlay()
+        {
+
         }
 
     }
